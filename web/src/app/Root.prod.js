8e12d0c4d72e5d901815bf5dd70aaed7390ddcad
Dani carmona serrat <danielcarmona55@gmail.com>
2017-03-03 22:52:04 +0100

    Added index, tools and routes

---
+++ web/src/app/Root.prod.js
@@ -0,0 +1,27 @@
+import React, { PropTypes } from 'react'
+import { Provider } from 'react-redux'
+import routes from './routes'
+import { Router } from 'react-router'
+//plugin needed for material-ui implementation, it fires
+//when you click on something, but it works muchbetter in mobile
+//than current react event. I will replace it as soon as react has
+// its own eventhandler for Tap
+import injectTapEventPlugin from 'react-tap-event-plugin';
+//theme needed for material-ui implementation
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+injectTapEventPlugin();
+
+const Root = ({ store, history }) => (
+  <Provider store={store}>
+    <MuiThemeProvider>
+      <Router history={history} routes={routes} />
+    </MuiThemeProvider>
+  </Provider>
+)
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
+}
+export default Root
