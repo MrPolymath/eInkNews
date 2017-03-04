4ee6acc81739c3417dc52de5aed8736346621e08
Dani carmona serrat <danielcarmona55@gmail.com>
2017-03-04 15:48:51 +0100

    Merge branch 'parsingHtml' to 'parsing'

---
+++ api/src/epub-to-html/elmundo.js
@@ -0,0 +1,61 @@
+var merge = require('lodash/merge');
+var jsdom = require("jsdom");
+var Promise =  require('bluebird')
+
+jsdom.env({
+  url: "http://www.elmundo.es/",
+  scripts: ["http://code.jquery.com/jquery.js"],
+  done: function (err, window) {
+    var $ = window.$;
+    var articles = []
+    $(".flex__item h3").each(function() {
+      var title = $(this).text()
+      var link = $(this).find('a').attr('href')
+      articles.push({title: title, link: link})
+    });
+    const articlePromises = articles.map(article => {
+      return getArticleContent(article).then(text => {
+        article.text = text.split('<h3 class="list-header"><span>')[0]
+        return article
+      })
+    })
+    Promise.all(articlePromises).then(articles => {
+      // articles have text here
+      console.log(articles);
+    })
+  }
+});
+
+function getArticleContent(article) {
+  const promise = new Promise((resolve, reject) => {
+    jsdom.env({
+      url: article.link,
+      scripts: ["http://code.jquery.com/jquery.js"],
+      done: function (err, window) {
+        var $ = window.$;
+        var text = ''
+        $('.date').nextAll().each(function() {
+          if ($(this).next().hasClass('subhead')) {
+            text = text + '<h3>' + $(this).next().text() + '</h3>'
+          }
+          text = text + $(this).html()
+        })
+        window.close();
+        resolve(text)
+      }
+    })
+  })
+  return promise
+}
+
+// const testArticle = {
+//   title: 'El testamento de Miguel Boyer: sólo les deja deudas a sus hijos',
+//   link: 'http://www.elmundo.es/loc/2017/03/04/58b964ea46163f990b8b4639.html'
+// }
+//
+// getArticleContent(testArticle)
+
+// var array1 = [{title: 'titulo1', link: 'link1'}, {title: 'titulo2', link: 'link2'}, {title: 'titulo3', link: 'link3'}]
+// var array2 = [{text: 'text1'}, {text: 'text2'}, {text: 'text3'}]
+// var result = merge([], array1, array2)
+// console.log(result);
