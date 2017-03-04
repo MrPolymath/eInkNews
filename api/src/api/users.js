5e17bc0bb72cc29d0f3cff592d81ca507d857bc1
Ian Martorell <ianmartorell@gmail.com>
2017-03-04 01:39:45 +0100

    Save list of interests

--- api/src/api/users.js
+++ api/src/api/users.js
@@ -4,9 +4,10 @@ import User from '../models/User'
 let user = Router()
 
 user.post('/', (req, res, next) => {
-  if (!req.body.email) {
+  if (!req.body.email || !req.body.interests) {
     return res.json('you suck')
   }
+
   User.findOne({ email: req.body.email }, (err, user) => {
     if (user == null) {
       const newUser = new User({
@@ -19,6 +20,13 @@ user.post('/', (req, res, next) => {
         })
         .catch(e => next(e));
     } else {
+      if (user.interests != req.body.interests) {
+        user.interests = req.body.interests
+        user.save((err) => {
+          if (err) return res.json('well this is embarrassing')
+          // TODO: Interests have changed, recreate bundle
+        })
+      }
       return res.json(user.getBundleUrl())
     }
   })
