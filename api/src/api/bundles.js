de3f638852927ab521d147a7e7bf50b320f03309
Ian Martorell <ianmartorell@gmail.com>
2017-03-04 03:05:45 +0100

    Don't generate the bundle in the bundle GET endpoint

--- api/src/api/bundles.js
+++ api/src/api/bundles.js
@@ -1,12 +1,11 @@
 import { Router } from 'express'
-import epubToHtml from '../epub-to-html'
 
 let bundle = Router()
 
 bundle.get('/:userId', (req, res) => {
   // TODO: Return this user's bundle.
   // If it's old, create a new one first.
-  epubToHtml('http://nytimes.com')
+  res.json('herp derp')
 })
 
 export default bundle
