2dc5fd7909cacee335fa9833bc812ae6fa2f8727
hacalox <hanadonc@gmail.com>
2017-03-23 09:34:25 +0100

    Reformat api/src/index.js

--- api/src/index.js
+++ api/src/index.js
@@ -8,14 +8,7 @@ import api from './api'
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-// Allow cross domain
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*")
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With")
-//   // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-//   // res.header('Access-Control-Allow-Headers', 'Content-Type')
-//   next()
-// })
+//CORS configuration
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
