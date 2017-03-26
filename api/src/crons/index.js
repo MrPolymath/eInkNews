72264aa5d33c8413cfba83771d4043c5c31f2bea
Dani carmona serrat <danielcarmona55@gmail.com>
2017-03-26 14:23:33 +0200

    Add ebook creation to crons index function
    Right now it's using the news-parser createEbook function. This function reeds to be refactored completely so that insted of getting an ebook type and an array of subscriptions it only receives the source name

--- api/src/crons/index.js
+++ api/src/crons/index.js
@@ -1,11 +1,32 @@
 const CronJob = require('cron').CronJob
 
-const job = new CronJob({
-  cronTime: '0 */6 * * *',
-  onTick: function() {
-    // Es corre cada 6 hores
-  },
-  start: false,
-  timeZone: 'America/Los_Angeles'
-});
-job.start();
+import schedules from './schedules.js'
+import createEbook from '../news-parser'
+
+schedules.forEach((schedule) => {
+  const source = schedule.source
+  const cronTime = schedule.cronTime
+  const timeZone = schedule.timeZone
+
+  new CronJob({
+    cronTime: cronTime,
+    onTick: function() {
+      // Es corre cada 6 hores
+      
+      // Ara mateix la funció createEbook llegia el bundleType i les subscriptions d'un user per a crear el ebook corresponent
+      // a les seves necessitats.
+      // Cal fer de nou el index.js de news-parser per a que ara el que revi sigui només el nom del source del que s'ha de crear
+      // el bundle
+      createEbook({
+        bundleType: 'epub',
+        subscriptions: source
+      })
+      createEbook({
+        bundleType: 'mobi',
+        subscriptions: source
+      })
+    },
+    start: true,
+    timeZone: timeZone
+  });
+})
