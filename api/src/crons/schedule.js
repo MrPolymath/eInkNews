4445bf603383157b7a93d8161f6c55ced247f7d3
Dani carmona serrat <danielcarmona55@gmail.com>
2017-03-26 13:45:20 +0200

    Add cron schedule file
    Simple structure that is up to change in the near future

---
+++ api/src/crons/schedule.js
@@ -0,0 +1,10 @@
+
+// The schedule object saves the times at which each source bundle should be updated
+
+const schedule = {
+  elmundo: '0 */6 * * *',
+  hackernews: '0 */6 * * *',
+  newyorktime: '0 */6 * * *'
+}
+
+export default schedule;
