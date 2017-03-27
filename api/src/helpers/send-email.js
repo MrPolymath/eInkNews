bf6f1cb95e9625fae5157b15546fddafdad6f15a
hacalox <hanadonc@gmail.com>
2017-03-27 02:12:11 +0200

    Refactor helper/send-email.js
    This helper now returns a promise, so it can be used
    as a synchronous function. Refactor done to get a bit
    more workflow control

--- api/src/helpers/send-email.js
+++ api/src/helpers/send-email.js
@@ -1,21 +1,26 @@
 const api_key = process.env.MAILGUN_API_KEY;
 const domain = process.env.MAILGUN_DOMAIN
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+import Promise from 'bluebird'
 
 const sendEmail = (email, content) => {
-  var data = {
-    from: 'Eink News <postmaster@eink.news>',
-    to: email,
-    subject: `${content.subject}`,
-    html: `${content.html}`
-  };
-  mailgun.messages().send(data, function (error, body) {
-    if (error){
-console.log(error);
-    }
-    console.log(body);
-  });
+  return new Promise((resolve) => {
+    var data = {
+      from: 'Eink News <postmaster@eink.news>',
+      to: email,
+      subject: `${content.subject}`,
+      html: `${content.html}`
+    };
+    mailgun.messages().send(data, function (error, body) {
+      if (error){
+        console.log(error);
+      }
+      console.log(body);
+      resolve(true)
+    });
 
+  })
 }
+
 
 export default sendEmail;
