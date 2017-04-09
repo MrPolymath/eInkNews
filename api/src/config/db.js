1ddb6ce9f351a49d58e5884e0a1e1681a4bab5ca
hacalox <hanadonc@gmail.com>
2017-04-10 00:14:24 +0200

    Make mongodb Atlas
    Mongodb atlas implementation.
    Atlas seems a nice option for MongoDb hosting service.
    It has a nice free tier, and when it's upgraded to paying plan still has good reviews
    and it's directly provided from the mongodb team.

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -3,9 +3,11 @@ import Promise from 'bluebird'
 
 mongoose.Promise = Promise
 
-const { MONGO_HOST, MONGO_PORT, MONGO_DB_NAME } = process.env
-
-const mongoUri = `${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`
+const { MONGO_HOST, MONGO_PORT, MONGO_DB_NAME, MONGO_USER, MONGO_PASSWORD} = process.env
+let mongoUri = `${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`
+if (MONGO_HOST != 'localhost'){
+  mongoUri = `${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}`
+}
 
 mongoose.connect(mongoUri, { server: { socketOptions: { keepAlive: 1 } } })
 mongoose.connection.on('error', () => {
