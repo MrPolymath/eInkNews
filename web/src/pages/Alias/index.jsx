634b16876f90679f3c0bdb4d3f49982d4e19eac5
hacalox <hanadonc@gmail.com>
2017-04-14 00:54:57 +0200

    Make new pages structure
    It has no sense having a folder for each page instead of a folder grouping all of them.

---
+++ web/src/pages/Alias/index.jsx
@@ -0,0 +1,41 @@
+
+import React, { Component, PropTypes } from 'react'
+import styles Alias.css';
+
+
+export default Alias extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+        currentstate: stateValue
+    }
+    //this.changeBackground = this.changeBackground.bind(this)
+  }
+    //changeBackground(evt){
+        //this.setState({
+          //currentstate  = evt.target.value
+        //})
+      //}
+
+
+  //AHORA ESTO NO IMPORTA
+  //static propTypes = {
+    //mobile: PropTypes.boolen.isRequired
+  //}
+  //static defaultProps = {
+    //mobile: false
+  //}
+
+
+  render() {
+      // Js functions and JS conditions using this.props.nameProp
+
+    return (
+      <div>
+        {/* <input type=text value={this.state.currentstate} onChange={changeBackground}/> */}
+
+      </div>
+    )
+  }
+}
