f17fa5c40a19d6ddcae5ac68f3c01f128e4a8379
hacalox <hanadonc@gmail.com>
2017-04-16 03:38:39 +0200

    Add nickname page response when submitting

--- web/src/pages/Nickname/index.jsx
+++ web/src/pages/Nickname/index.jsx
@@ -4,7 +4,7 @@ import TextInput from '../../common/components/TextInput.jsx'
 import SubmitButton from '../../common/components/SubmitButton.jsx'
 import style from '../../common/common.css'
 import {updateNickname} from '../../middleware/api'
-
+import {Notification} from './Notification'
 export default class Nickname extends Component {
   constructor(props){
     super(props)
@@ -16,29 +16,54 @@ export default class Nickname extends Component {
         email: email,
         response: ''
     }
-    this.changeState = this.changeState.bind(this)
+    this.changeNickname = this.changeNickname.bind(this)
     this.updateNickname = updateNickname.bind(this)
+    this.changeResponse = this.changeResponse.bind(this)
   }
-    changeState(evt){
+    changeNickname(evt){
       this.setState({nickname: evt.target.value})
     }
-
+    changeResponse(){
+      this.setState({response: ''})
+    }
   render() {
     return (
       <Grid style={style}>
+      {this.state.response === '' ? (
         <Row>
           <Col xs={12}>
             <h1 style={{color: 'white', marginTop:'100px'}}>Just one step away from your first bundle.</h1>
             <p style={{color: 'white', marginTop:'100px',fontSize: '20px'}}> You will use this nickname to download your ebook bundles, so please write down a nickname you will remember.</p>
-            <TextInput label='Your Nickname' handleChange={this.changeState} />
+            <TextInput label='Your Nickname' handleChange={this.changeNickname} />
           </Col>
           <Col xs={12}>
             <SubmitButton
               label="Send"
-              handleTap={this.updateNickname(this.state.email, this.state.id, this.state.nickname)}
+              handleTap={this.updateNickname}
             />
           </Col>
         </Row>
+      ) : (
+        <Row>
+          <Col xs={12}>
+            {this.state.response === (403||400) &&
+              <Notification
+                label='Sorry, an error has ocurred. Please click on the email link again and if this error keeps popping up please contact us at aerosdevs@gmail.com'
+                handleTap={this.changeResponse}
+              />}
+            {this.state.response === 200 &&
+              <Notification
+                label={'Done, now you can download your first ebook bundle going at www.eink.news/d/'+this.state.nickname}
+                handleTap={this.changeResponse}
+              />}
+            {this.state.response === 202 &&
+              <Notification
+                label='This nickname is already in use, please try another one'
+                handleTap={this.changeResponse}
+              />}
+            </Col>
+          </Row>
+        )}
       </Grid>
     )
   }
