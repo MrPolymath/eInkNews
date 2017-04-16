fe06a8b2c419fecf0fd36d5f46780558d6229304
hacalox <hanadonc@gmail.com>
2017-04-16 03:37:28 +0200

    Fix SubmitButton handleTap

--- web/src/common/components/SubmitButton.jsx
+++ web/src/common/components/SubmitButton.jsx
@@ -4,7 +4,7 @@ import RaisedButton from 'material-ui/RaisedButton'
 const SubmitButton = (props) => (
   <RaisedButton
     label={props.label}
-    onTouchTap={() => props.handleTap}
+    onTouchTap={props.handleTap}
     primary={true}
     style={{marginTop:'60px',  borderRadius:'20px' }}
     labelStyle={{fontSize:'20px',height: '40px', marginTop:'10px',  borderRadius:'20px' }}
